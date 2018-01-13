c87a7fae22b08b914a38914ba033d9062c98df74
johnvoyage <jmyork762@gmail.com>
2018-01-13 11:13:47 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return "#{array[0]}"
+  elsif array.count == 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    return_str = ""
+    for i in 0...array.count - 2
+      return_str += "#{array[i]}, "
+    end
+    return_str += "#{array[-2]}, and #{array[-1]}"
+    return_str
+  end
 end
